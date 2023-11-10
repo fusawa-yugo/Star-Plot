68addc79bd673dca62c3f13c1cb4135f322bc66e
fusawayugo <fusawayugo0712@g.ecc.u-tokyo.ac.jp>
2023-11-10 20:58:55 +0900

    a

--- dot_matching/function.py
+++ dot_matching/function.py
@@ -9,5 +9,22 @@ def csv_to_data(filename):
     HIPnum=df['HIPnum'].values
     return x,y,lum,HIPnum
 
+
 def calc_dist(p1,p2):
     return np.sqrt(sum((p1-p2)**2))
+
+
+def calc_theta(x,y):
+    if x==0:
+        if y<0:
+            return (-1)*np.pi/2
+        else:
+            return np.pi/2
+    theta=np.argtan(y/x)
+    if x<0:
+        if y>=0:
+            theta+=np.pi
+        else:
+            theta-=np.pi
+    return theta
+
